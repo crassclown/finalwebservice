3e2dd0d8b8de256fb766b4dc5c5fb8a16eedc65a
Hendry <hendryrpl@gmail.com>
2018-06-13 16:57:47 +0700

    nambahin migration file

---
+++ database/migrations/2018_06_01_125349_create_mahasiswa_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMahasiswaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nim', 10)->unique();
+            $table->string('nama');
+            $table->string('telp');
+            $table->string('email', 100)->unique();
+            $table->text('alamat');
+            $table->char('fakultas', 2);
+            $table->char('prodi', 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+}
